96ffcf791147afe6eba2b5df3f4144b1dec7f452
Brayden <brayden.w491@stu.nebo.edu>
2019-02-20 10:29:27 -0700

    so not working but still fixable

--- Adventure_Game/game.js
+++ Adventure_Game/game.js
@@ -35,7 +35,7 @@ switch(outside){
     case "gttfd":
         alert("You walk up to the front door and knock but nobody comes. There is large chatter inside so maybe they didn't hear you... You knock again and still nobody comes. You decide to invite yourself in. As you walk in you fail to see the man hiding in the shadows. He jumps you and being un able to stop him, he kills you... game over.");
         break;
-    
+        
     case "go through the back pathway":
     case "gttbp":
         var decide = prompt("The pathway was a gravel path that led all the way to the back door. Well walking past you couldn't help but see a pipe on the ground. \n - Take pipe \n - Keep walking");
@@ -63,78 +63,33 @@ switch(outside){
                     else if(Sit == "look at jackson"){
                         alert("As soon as you look up at Jackson he shoots you back with a smile and a 'hi.' He appears to be a nerd but afraid to make any assumptions you look away." );
                     }
+                    else if(Sit == "look at tyler"){
+                        alert("Tyler is a big muscular boy who appears like he isn't scared to crack a few skulls. Both of his arms have full sleeve tattoos of angels and demons. He looks like a mean dude but his smile is somewhat inviting.");
+                    }
+                    
+                    else if(Sit == "continue with the party" || Sit == "continue" || Sit == "cwtp"){
+                        var bored = prompt("Sara yells, lets get this party started! and pulls out a bored game from her backpack. At the same time Jeremy pulls one out of his bag. Somehow in all the commotion they leave you to pick whos bored game to play... \n - play saras game \n - play jeremys game")
+                        
+                        if (bored == "play saras game" || bored == "psg"){
+                            alert("Sara brought Candy Land o the party! You had a nice enjoyable time at the party and went home happy. The end");
                 }
+                        else if (bored == "play jeremys game" || bored == "pjg"){
+                            
+                        }
+                    }
                 else if(Enter == "leave" || Enter == "l"){
-                   alert("You frantically shut the door and run away. Nobody knows why you just decided to leave some people theorise that you couldn't take the social pressure. Either way you aren't at the party so I guess... game over.");
+                   alert("You frantically shut the door and run away. Nobody knows why you just decided to leave some people theorise that you couldn't take the social pressure. Either way you aren't at the party so I guess... The end.");
                 }
                 
                 
-            }
             else if (Decide == "leave party" || "lp"){
                 alert("You decide to leave the party so you get back in your car start the engine and head home. Back to your boring life, sad and alone... As you contimplaite your meaning in life you fail to see the turn ahead of you and drive right into a firehydrant ending your life... Game over");
             }
         }
         
-        
         break;
-
-
-     /*    
-    case "keep walking":
-    case "kw":
-        prompt("As you walk forward you can see lights coming from the windows. It's not to big of a party maybe only around 6 people but it's perfect for you nonetheless. You're at the back door. \n - enter \n - leave party");    
-        
-    case "take pipe":
-        alert("As you reach for the pipe you failed to notice the loose rock sticking out of the ground... Tripping you try to catch yourself but fail and fall face first into an electric box... Game over!");  
-        break;
-        
-         case "leave party":
-        alert("You decide to leave the party so you get back in your car start the engine and head home. Back to your boring life, sad and alone... As you contimplaite your meaning in life you fail to see the turn ahead of you and drive right into a firehydrant ending your life... Game over");
-        break;
-         
-    case "enter":
-    case "e":
-        prompt("As you enter the house the door makes a creaking noise notifing/freaking out the other 6 people in the room. After they recognize that it's just you they all settle down. Jeremy (Your best friend and the only reason you knew about this party) greets you and calls you to sit with them. \n - Sit down with them \n - leave");
-        break;
-        
-    case "leave":
-        alert("You frantically shut the door and run away. Nobody knows why you just decided to leave some people theorise that you couldn't take the social pressure. Either way you aren't at the party so I guess... game over.");
-        break;
-        
-    case "sit down with them":
-        prompt("You decide to sit down with the group and decide to sit next to Jeremy. He slowly introduces you to the group. Out of the seven of you (including you) there are 3 girls and 4 guys. There is Sara Jeremys girlfriend, brooklyn, Mei, Jackson and Tyler. \n - Look at (put name) \n - continue with the party");
-        break;
-        
-    case "look at Sara":
-        alert("Sara was a cute short girl with bright red hair. She smiled at you as you looked up at her.");
-
-    case "look at brooklyn":
-        alert("Brooklyn is a cold white girl dressed in all black with black makeup. She seems cheerful and comfortable at this party but overall is off putting.");
-
-    case "look at Mei":                        
-        alert("Mei is a nice cheerful girl that has these bright green eyes and long black hair. She smiles at you with a crooked smile. There is a eager fire in her eyes. You look forward to meeting her later.");
-                                
-    case "Look at Jackson":                        
-        alert("As soon as you look up at Jackson he shoots you back with a smile and a 'hi.' He appears to be a nerd but afraid to make any assumptions you look away." );
-                            
-    case "look at Tyler":                        
-        alert("Tyler is a big muscular boy who appears like he isn't scared to crack a few skulls. Both of his arms have full sleeve tattoos of angels and demons. He looks like a mean dude but his smile is somewhat inviting.");
-        
-    case "look at Jeremy":
-        alert("Jeremy looks up at you as you you look at him. He just smiles that friendly smile that youve known since elementary school. Hes a cool guy and you have a lot of respect for him." );
-        
-    case "continue with the party":
-        prompt("Sara yells, lets get this party started! and pulls out a bored game from her backpack. At the same time Jeremy pulls one out of his bag. Somehow in all the commotion they leave you to pick whos bored game to play... \n - play saras game" );
-    
-
-
-        break;
-        break;
-        break;
-        break;
-        break;
-        break;
-   */
 }
     }
 }
+    }
+    

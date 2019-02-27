5896c932714c56572ab453ee7c1705c749e91c17
Brayden <brayden.w491@stu.nebo.edu>
2019-02-27 10:30:01 -0700

    Idk you just gotta fix it all

--- Adventure_Game/game.js
+++ Adventure_Game/game.js
@@ -1,4 +1,9 @@
 Game();
+
+var inventory = {
+    whatever:0,
+}
+
 function Game (){
     alert("Super House Party");
     var playerName = prompt("What is your name?");
@@ -25,42 +30,45 @@ function Game (){
                     alert("As you reach for the pipe you failed to notice the loose rock sticking out of the ground... Tripping you try to catch yourself but fail and fall face first into an electric box... Game over!");
                 }
                 else if(decide == "keep walking" || decide == "kw"){
-                    var Decide = prompt("As you walk forward you can see lights coming from the windows. It's not to big of a party maybe only around 6 people but it's perfect for you nonetheless. You're at the back door. \n - enter \n - leave party");
-                if(Decide == "leave party" || Decide == "lp"){
+                    var decidee = prompt("As you walk forward you can see lights coming from the windows. It's not to big of a party maybe only around 6 people but it's perfect for you nonetheless. You're at the back door. \n - enter \n - leave party");
+                if(decidee == "leave party" || decidee == "lp"){
                     alert("You decide to leave the party so you get back in your car start the engine and head home. Back to your boring life, sad and alone... As you contimplaite your meaning in life you fail to see the turn ahead of you and drive right into a firehydrant ending your life... Game over");
                 }
-                    else if(Decide == "enter" || Decide == "e"){
-                        var Enter = prompt("As you enter the house the door makes a creaking noise notifing/freaking out the other 6 people in the room. After they recognize that it's just you they all settle down. Jeremy (Your best friend and the only reason you knew about this party) greets you and calls you to sit with them. \n - Sit down with them \n - leave");
+                    else if(decidee == "enter" || decidee == "e"){
+                        var enter = prompt("As you enter the house the door makes a creaking noise notifing/freaking out the other 6 people in the room. After they recognize that it's just you they all settle down. Jeremy (Your best friend and the only reason you knew about this party) greets you and calls you to sit with them. \n - Sit down with them \n - leave");
                         
-                        if(Enter == "leave" || Enter == "l"){
+                        if(enter == "leave" || enter == "l"){
                             alert("You frantically shut the door and run away. Nobody knows why you just decided to leave some people theorise that you couldn't take the social pressure. Either way you aren't at the party so I guess... The end.");
                         }
-                        else if(Enter == "sit down with them" || Enter == "sdwt"){
-                            var Sit = prompt("You decide to sit down with the group and decide to sit next to Jeremy. He slowly introduces you to the group. Out of the seven of you (including you) there are 3 girls and 4 guys. There is Sara Jeremys girlfriend, brooklyn, Mei, Jackson and Tyler. \n - Look at (put name) \n - continue with the party");
-                            if (Sit == "look at sara"){
+                        else if(enter == "sit down with them" || enter == "sdwt"){
+                            var sit = prompt("You decide to sit down with the group and decide to sit next to Jeremy. He slowly introduces you to the group. Out of the seven of you (including you) there are 3 girls and 4 guys. There is Sara Jeremys girlfriend, brooklyn, Mei, Jackson and Tyler. \n - Look at (put name) \n - continue with the party");
+                            if (sit == "look at sara"){
                                 alert("Sara was a cute short girl with bright red hair. She smiled at you as you looked up at her.");
                             }
-                            else if (Sit == "look at brooklyn"){
+                            else if (sit == "look at brooklyn"){
                                 alert("Brooklyn is a cold white girl dressed in all black with black makeup. She seems cheerful and comfortable at this party but overall is off putting.");
                             }
-                            else if (Sit == "look at mei"){
+                            else if (sit == "look at mei"){
                                 alert("Mei is a nice cheerful girl that has these bright green eyes and long black hair. She smiles at you with a crooked smile. There is a eager fire in her eyes. You look forward to meeting her later.");
                             }
-                            else if (Sit == "look at jackson"){
+                            else if (sit == "look at jackson"){
                                 alert("As soon as you look up at Jackson he shoots you back with a smile and a 'hi.' He appears to be a nerd but afraid to make any assumptions you look away." );
                             }
-                            else if (Sit == "look at tyler"){
+                            else if (sit == "look at tyler"){
                                 alert("Tyler is a big muscular boy who appears like he isn't scared to crack a few skulls. Both of his arms have full sleeve tattoos of angels and demons. He looks like a mean dude but his smile is somewhat inviting.");
                             }
-                            else if(Sit == "continue with the party" || Sit == "cwtp"){
+                            else if(sit == "continue with the party" || Sit == "cwtp"){
                                 var bored = prompt("Sara yells, lets get this party started! and pulls out a board game from her backpack. At the same time Jeremy pulls one out of his bag. Somehow in all the commotion they leave you to pick whos bored game to play... \n - play saras game \n - play jeremys game");
                                 
                                 if(bored == "play saras game" || bored == "psg"){
-                                    alert("Sara brought Candy Land o the party! You had a nice enjoyable time at the party and went home happy. The end");
+                                    alert("Sara brought Candy Land to the party! You had a nice enjoyable time at the party and went home happy. The end");
                                 }
                                 else if (bored == "play jeremys game" || "pjg"){
-                                    alert("YOU DID IT!");
+                                    alert("He brought an ouija bored...");
+                                    Ouija();
                                 }
+                                
+                                
 
                             }
                         }
@@ -72,4 +80,10 @@ break;
         }
 
     }
+    function Ouija(){
+        var oija  = prompt("The whole room feels darker when Jeremy sets it on the ground. There is this unsettling feeling through out the entire room. Sara decides to dim the lights real dark to better set the mood. They asks who wants to join in on the first set of rounds. \n - join in \n - sit back");
+        
+        if(oija == "join in" || oija == "ji")
+        
+
 }
